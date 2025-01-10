e51f6f6bd83d130993ba555cff02ff39cd852004
George-25 <georgemallory26@gmail.com>
2025-01-09 20:40:05 -0500

    updated search bar with state and event handles with value passed on it

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
+
 import React from "react";
 
 const SearchBar = ({ onSubmit }) => {
+  //state part
+  const [term, setTerm] = useState(" ");
+  //event handle for form submit part
   const handleFormSubmit = (event) => {
     event.preventDefault();
     onSubmit("cars");
   };
+  // handle change part
+  const handelChange = (event) => {
+    setTerm(event.target.value);
+  };
+
   return (
     <div>
-      <form>
-        <input onSubmit={handleFormSubmit} />
+      <form onSubmit={handleFormSubmit}>
+        <input value={term} onChange={handelChange} />
       </form>
     </div>
   );
